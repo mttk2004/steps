73dd44140e26af6863afc17419db87d322532efc
= <=>
2024-07-22 20:38:38 +0700

    v2.0 second commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,39 +10,54 @@ import { useState } from 'react';
 
 
 const messages = [
-	"Learn React ⚛️",
-	"Apply for jobs 💼",
-	"Invest your new income 🤑",
+	'Learn React ⚛️',
+	'Apply for jobs 💼',
+	'Invest your new income 🤑',
 ];
 
 export default function App() {
-	const [step, setStep] = useState(1)
+	return <div>
+		<Steps/>
+		<Steps/>
+	</div>
+}
+
+function Steps() {
+	const [step, setStep] = useState(1);
+	const [isOpen, setIsOpen] = useState(true);
 	
 	const buttonsStyle = {
-		color: '#fff',
+		color          : '#fff',
 		backgroundColor: '#7950f2'
-	}
+	};
 	
-	const handlerPrevious = function() {
-		if (step > 1) setStep(step - 1)
-	}
+	const handlerPrevious = function () {
+		if (step > 1) setStep(step => step - 1);
+	};
 	
-	const handlerNext = function() {
-		if (step < 3) setStep(step + 1)
-	}
+	const handlerNext = function () {
+		if (step < 3) setStep(step => step + 1);
+	};
 	
-	return <div className="steps">
-		<div className="numbers">
-			<div className={step >= 1 ? 'active' : ''}>1</div>
-			<div className={step >= 2 ? 'active' : ''}>2</div>
-			<div className={step >= 3 ? 'active' : ''}>3</div>
-		</div>
-		
-		<p className="message">Step {step}: {messages[step-1]}</p>
-		
-		<div className="buttons">
-			<button style={buttonsStyle} onClick={handlerPrevious}>Previous</button>
-			<button style={buttonsStyle} onClick={handlerNext}>Next</button>
-		</div>
+	return <div>
+		<button className="close" onClick={() => setIsOpen(isOpen => !isOpen)}>
+			{isOpen ? <>&times;</> : <>&rarr;</>}
+		</button>
+		{isOpen &&
+		 <div className="steps">
+			 <div className="numbers">
+				 <div className={step >= 1 ? 'active' : ''}>1</div>
+				 <div className={step >= 2 ? 'active' : ''}>2</div>
+				 <div className={step >= 3 ? 'active' : ''}>3</div>
+			 </div>
+			 
+			 <p className="message">Step {step}: {messages[step - 1]}</p>
+			 
+			 <div className="buttons">
+				 <button style={buttonsStyle} onClick={handlerPrevious}>Previous</button>
+				 <button style={buttonsStyle} onClick={handlerNext}>Next</button>
+			 </div>
+		 </div>
+		}
 	</div>;
 }
